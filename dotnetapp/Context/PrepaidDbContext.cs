b0abf1bb9b4eb0ea1a7ee626e394e031a9d5b3a3
jeyakumark7 <jeyakumar31102001@gmail.com>
2023-07-25 14:28:02 +0000

    Backup Commit

---
+++ dotnetapp/Context/PrepaidDbContext.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetapp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetapp.Context
+{
+    public class PrepaidDbContext:DbContext
+   {
+    public DbSet<Prepaid> Prepaid { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Login> Logins { get; set; }
+    public DbSet<Addons> Addons {get; set;}
+    public DbSet<Recharge> Recharge {get; set;}
+    public DbSet<Review> Review{get;set;}
+    
+
+     public PostPaidDbContext(DbContextOptions<PostPaidDbContext> options) : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+            modelBuilder.Entity<PostPaid>().ToTable("Prepaid");
+            modelBuilder.Entity<User>().ToTable("users");
+            modelBuilder.Entity<Admin>().ToTable("admins");
+            modelBuilder.Entity<Login>().ToTable("logins");
+            modelBuilder.Entity<Addons>().ToTable("Addons");
+            modelBuilder.Entity<Recharge>().ToTable("Recharge");
+            modelBuilder.Entity<Review>().ToTable("Review");
+
+    }
+   }
+  
+}
