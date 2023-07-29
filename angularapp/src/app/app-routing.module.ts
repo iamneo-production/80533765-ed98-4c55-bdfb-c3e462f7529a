fe98fef71c8caf08bc0d9d535da22fcffd0a9f66
jeyakumark7 <jeyakumar31102001@gmail.com>
2023-07-29 07:16:17 +0000

    Routing

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrepaidComponent } from './prepaid/prepaid.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedService } from './services/shared.service';
 
 
 
 const routes: Routes = [
   {path:'adminPrePaid',component:PrepaidComponent},
+  {path:'', redirectTo: '/adminPrePaid',pathMatch:'full'}
 ];
 
 @NgModule({
