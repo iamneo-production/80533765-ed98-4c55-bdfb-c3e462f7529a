152bb283cdda4f7181a43732472de44056d5e388
Dharani1004 <dharusivadharu@gmail.com>
2023-07-30 15:08:17 +0000

    workspace

--- dotnetapp/Migrations/20230730150356_InitialSetup.Designer.cs
+++ dotnetapp/Migrations/20230730150356_InitialSetup.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dotnetapp.Context;
 
 namespace dotnetapp.Migrations
 {
     [DbContext(typeof(PostPaidDbContext))]
-    partial class PostPaidDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230730150356_InitialSetup")]
+    partial class InitialSetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
